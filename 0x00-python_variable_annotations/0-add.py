48af156e501bbb0baf1404b397bbff0485eb51ca
NegasiAbe <negasiabebe@gmail.com>
2023-04-29 01:19:59 +0200

    solution updated

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-'''Task 0's module.
+'''Task 0's module is done.
 '''
 
 
 def add(a: float, b: float) -> float:
-    '''Adds two floating-point numbers.
+    '''a function which adds two float numbers a and b.
     '''
     return a + b
